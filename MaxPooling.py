7c82b16d34edd25d09b87a4043dff1e7b1ee6d63
James_Aks <jamesakindele08@gmail.com>
2025-06-09 16:41:18 +0100

    Update MaxPooling
    Added backpropagation. Removed the axis from the max function as its finding the max of a 2D array

--- MaxPooling.py
+++ MaxPooling.py
@@ -18,9 +18,10 @@ class MaxPooling(Layer):
     def forward(self,input):
 
         # ((input size - pooling window size) / stride ) + 1 Assuming no padding
+        #self.input = input
         output_width = ((self.input_shape[0] - self.window[0]) / self.stride) + 1
         output_hieght =((self.input_shape[1] - self.window[1]) / self.stride) + 1
-        output = np.zero(output_width, output_hieght, self.input_shape[2])
+        self.output = np.zero(output_width, output_hieght, self.input_shape[2])
 
         for d in range(self.input_shape[2]):
 
@@ -29,15 +30,32 @@ class MaxPooling(Layer):
                  for h in range(output_hieght):
                     
                     # Finding the max in an n x m sized window at depth d
-                    output[w,h,d] = input[w*self.window[0]:w*self.window[0] + self.window[0], h*self.window[1]: h*self.window[1] + self.window[1],d] .max(axis = (0,1))
+                    
+                    self.output[w,h,d] = input[w*self.window[0]:w*self.window[0] + self.window[0], h*self.window[1]: h*self.window[1] + self.window[1],d] .max()
 
-        return output
+                    self.max_positions = np.where(input == self.output[w,h,d])
+
+        return self.output
     
     
-    def backward(self, output_grad,):
+    def backward(self, output_grad):
 
         # Gradient only matter only on the positions where the max was found everything else is left the same so pad out with zero.
+        #Initialise an array of zeroes similar to the input
+        backprop = np.zeros(self.input_shape)
 
-        return
+        for d in range(self.input_shape[2]):
+
+            for w in range(self.input_shape[0]):
+
+                 for h in range(self.input_shape[1]):
+                    
+                    # Finding the max in an n x m sized window at depth d
+                    # Adds the error gradient to the array only where the max is found. Boolean at the end determines whether its is added or not as the max is either found there (1) or not (0).
+                    
+                    backprop[w*self.window[0]:w*self.window[0] + self.window[0], h*self.window[1]: h*self.window[1] + self.window[1],d] = output_grad[w:w+1,h:h+1,d] * (self.output[w:w+1,h:h+1,d] == self.input[w*self.window[0]:w*self.window[0] + self.window[0], h*self.window[1]: h*self.window[1] + self.window[1],d] .max())
+
+            return backprop
+
         
 
